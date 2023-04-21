f4c84618edfbe96afb7408bb4b49714725ad77f2
ccristi08 <64239697+ccristi08@users.noreply.github.com>
2023-04-21 04:01:06 -0500

    Add files via upload

--- Pan-tilt.ino
+++ Pan-tilt.ino
@@ -1,10 +1,13 @@
 #include <Servo.h>
 
-Servo servo1;
-Servo servo2;
+Servo servo1; // Paning servo, to move left and right
+Servo servo2; // Tilting servo, to move up and down
 
 int pos1 = 90; // initial position of servo 1
 int pos2 = 90; // initial position of servo 2
+// pos start/end are used to make the movement smoother
+int pos_start;
+int pos_end;
 
 void setup() {
   Serial.begin(9600);
@@ -15,16 +18,43 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     char c = Serial.read();
+
     if (c == 'a') {
-      pos1 = pos1 + 30;
-      servo1.write(pos1);
+      pos_start = pos1;
+      pos_end = pos1 + 30;
+
+      for (int i = pos_start; i <= pos_end; i++) {
+        servo1.write(i);
+        delay(15); // small delay between each iteration. It determines how "slow" it moves
+      }
+      pos1 = pos_end; // update current position of servo 1
     } else if (c == 'd') {
-      pos1 = pos1 - 30;
-      servo1.write(pos1);
+      pos_start = pos1;
+      pos_end = pos1 - 30;
+
+      for (int i = pos_start; i >= pos_end; i--) {
+        servo1.write(i);
+        delay(15); // small delay between each iteration. It determines how "slow" it moves
+      }
+      pos1 = pos_end; // update current position of servo 1
     } else if (c == 'w') {
-      servo2.write(0);
+      pos_start = pos2;
+      pos_end = pos2 + 30;
+
+      for (int i = pos_start; i <= pos_end; i++) {
+        servo2.write(i);
+        delay(15); // small delay between each iteration. It determines how "slow" it moves
+      }
+      pos2 = pos_end; // update current position of servo 2
     } else if (c == 's') {
-      servo2.write(180);
+      pos_start = pos2;
+      pos_end = pos2 - 30;
+
+      for (int i = pos_start; i >= pos_end; i--) {
+        servo2.write(i);
+        delay(15); // small delay between each iteration. It determines how "slow" it moves
+      }
+      pos2 = pos_end; // update current position of servo 2
     }
   }
 }
